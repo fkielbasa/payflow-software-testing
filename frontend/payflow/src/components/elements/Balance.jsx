84ba3ed93dc7d1c9c84b2e0ec467040db4c5f6ae
Narothe <kuba.dojka@gmail.com>
2023-12-04 12:31:42 +0100

    small changes

--- frontend/payflow/src/components/elements/Balance.jsx
+++ frontend/payflow/src/components/elements/Balance.jsx
@@ -2,31 +2,35 @@ import React from 'react';
 import '../styles/PagesCSS.css';
 
 function Balance(props) {
-    let balanceUSD = 27.37;
-    let accountNumberUSD = '3423870000441246075444';
-
     // account number formatting
-    const formattedAccountNumber = formatAccountNumber(accountNumberUSD);
+    const accountNumberConst = formatAccountNumber(props.accountNumber);
 
     function formatAccountNumber(accountNumber) {
+        const accountString = accountNumber.toString();
+
+        const firstTwoDigits = accountString.substring(0, 2);
+        const remainingDigits = accountString.substring(2);
+
         const chunks = [];
-        for (let i = 0; i < accountNumber.length; i += 4) {
-            chunks.push(accountNumber.substring(i, i + 4));
+        for (let i = 0; i < remainingDigits.length; i += 4) {
+            chunks.push(remainingDigits.substring(i, i + 4));
         }
-        return chunks.join(' ');
+
+        return `${firstTwoDigits} ${chunks.join(' ')}`;
     }
+
 
     return (
         <div className="usdBalance">
             <div className="balanceText">
                 <div>
                     <p className="textDecoration">Saldo {props.currency}</p>
-                    <p className="textDecoration, fontSize">USD ${balanceUSD}</p>
+                    <p className="textDecoration, fontSize">{props.currency} ${props.balance}</p>
                 </div>
                 <br />
                 <div>
                     <p className="textDecoration">Nr. rachunku:</p>
-                    <p className="textDecoration">{formattedAccountNumber}</p>
+                    <p className="textDecoration">{accountNumberConst}</p>
                 </div>
             </div>
         </div>
--- frontend/payflow/src/components/pages/HomePage.jsx
+++ frontend/payflow/src/components/pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/PagesCSS.css';
 import Balance from "../elements/Balance";
-import EurBalance from "../elements/EurBalance";
 
 function HomePage() {
     return (
         <div className="HomePage">
             <div className="balancePosition">
-                <Balance currency = {'USD'}/>
-                <EurBalance/>
+                <Balance currency = {'USD'} balance = {21.37} accountNumber={'3423870000441246075444'}/>
+                <Balance currency = {'EUR'} balance = {69.42} accountNumber={'3124150000843646841238'}/>
             </div>
             <div>
                 <div className="transaction">
