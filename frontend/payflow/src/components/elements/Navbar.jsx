6d3edca4e87ee329c3bd4847746e68f300b4b64b
Narothe <kuba.dojka@gmail.com>
2023-12-05 00:18:02 +0100

    not fully ready

--- frontend/payflow/src/components/elements/Navbar.jsx
+++ frontend/payflow/src/components/elements/Navbar.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import '../styles/NavbarStyles.css';
 
-import house from "../../assets/houses.svg";
-import houseFill from "../../assets/houses-fill.svg";
-import transactions from "../../assets/money-bill-transfer.svg";
-import transfers from "../../assets/arrow-right-arrow-left.svg";
-import credits from "../../assets/money-bill-fill.svg";
-import cards from "../../assets/credit-card.svg";
+import house from "../../assets/home/houses.svg";
+import houseFill from "../../assets/home/houses-fill.svg";
 
-import settings from "../../assets/gear.svg";
-import settingsFill from "../../assets/gear-fill.svg";
-import logOut from "../../assets/door-open.svg";
+import transactions from "../../assets/money-bill-transfer.svg";
+import { IoCashOutline } from "react-icons/io5";
+import { IoCash } from "react-icons/io5";
+
+
+
+
+import transfers from "../../assets/arrow-right-arrow-left.svg";
+
+import credits from "../../assets/credits/money-bill.svg";
+import creditsFill from "../../assets/credits/money-bill-fill.svg";
+
+import cards from "../../assets/cards/credit-card.svg";
+import cardsFill from "../../assets/cards/credit-card-fill.svg";
+
+import settings from "../../assets/settings/gear.svg";
+import settingsFill from "../../assets/settings/gear-fill.svg";
+
+import logOut from "../../assets/logOut/door-open.svg";
+import logOutFill from "../../assets/logOut/door-open-fill.svg";
 
 import Logo from "./Logo";
 
@@ -19,8 +32,14 @@ function Navbar() {
     const location = useLocation();
 
     const isHome = location.pathname === '/home';
+    const isTransactions = location.pathname === '/transactions';
+
+
+    const isCredits = location.pathname === '/credits';
+    const isCards = location.pathname === '/cards';
 
     const isSettings = location.pathname === '/settings';
+    const isLogOut = location.pathname === '/logOut';
 
 
     return (
@@ -43,7 +62,8 @@ function Navbar() {
                         </li>
                         <li>
                             <Link to='/transactions'>
-                                <img className="navImages" src={transactions} alt="transactions"/>
+                                {/*<img className="navImages" img={<IoCashOutline />} alt="transactions" />*/}
+                                <IoCashOutline />
                                 Transakcje
                             </Link>
                         </li>
@@ -55,13 +75,13 @@ function Navbar() {
                         </li>
                         <li>
                             <a href="/credits">
-                                <img className="navImages" src={credits} alt="credits"/>
+                                <img className="navImages" src={isCredits ? creditsFill : credits} alt="credits"/>
                                 Kredyty
                             </a>
                         </li>
                         <li>
                             <a href="/cards">
-                                <img className="navImages" src={cards} alt="cards"/>
+                                <img className="navImages" src={isCards ? cardsFill : cards} alt="cards"/>
                                 Karty
                             </a>
                         </li>
@@ -78,7 +98,7 @@ function Navbar() {
                     </li>
                     <li>
                         <Link to='/logOut'>
-                            <img className="navImages" src={logOut} alt="logOut"/>
+                            <img className="navImages" src={isLogOut ? logOutFill : logOut} alt="logOut"/>
                             Wyloguj
                         </Link>
                     </li>
