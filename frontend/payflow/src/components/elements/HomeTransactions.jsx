3b2904ebecfe2193e6778bca26d6e89729e4c10b
Narothe <kuba.dojka@gmail.com>
2023-12-09 17:19:52 +0100

    ostatnie transakcje

---
+++ frontend/payflow/src/components/elements/HomeTransactions.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../styles/HomeTransictionsStyles.css';
+import circleMinus from "../../assets/transations/circleMinus.png";
+import circlePlus from "../../assets/transations/circlePlus.png";
+
+
+
+function HomeTransictions(props) {
+    const balance = props.balance.toFixed(2);;
+    const type = props.type;
+    const acquired = props.acquired;
+    const place = props.place;
+    let source;
+    let symbol;
+
+
+    if (type === 'negative'){
+        source = circleMinus;
+        symbol = '-';
+    } else if (type === 'positive') {
+        source = circlePlus;
+        symbol = '+';
+    }
+
+    return (
+        <div className="shortPayment">
+            <div className="shortPaymentText">
+                <img className="transactionCircleMinus" src={source}/>
+                <div className="paymentTextPosition">
+                    <p className="transactionTextDecoration">{acquired}</p>
+                    <p className="transactionTextDecoration transactionTextSmall">{place}</p>
+                </div>
+            </div>
+            <p className="paymentTextSize">{symbol}${balance}</p>
+        </div>
+    );
+}
+
+export default HomeTransictions;
--- frontend/payflow/src/components/pages/Home.jsx
+++ frontend/payflow/src/components/pages/Home.jsx
@@ -3,8 +3,7 @@ import '../styles/HomeStyles.css';
 import Balance from "../elements/Balance";
 import TopNavbar from "../elements/TopNavbar";
 
-import HomeTransactionsMinus from "../elements/HomeTransactionsMinus";
-import HomeTransactionsPlus from "../elements/HomeTransictionsPlus";
+import HomeTransactions from "../elements/HomeTransactions";
 
 function Home() {
     return (
@@ -17,13 +16,13 @@ function Home() {
                 </div>
                 <div className="transactionPosition">
                     <div className="shortPayment">
-                        <p className="marginL">Transakcje</p>
+                        <p className="marginL">Ostatnie transakcje</p>
                         <p className="smallLetters">Pokaż więcej</p>
                     </div>
-                    <HomeTransactionsMinus balance={11.19}/>
-                    <HomeTransactionsMinus balance={23.18}/>
-                    <HomeTransactionsPlus balance={11.65}/>
-                    <HomeTransactionsMinus balance={21.37}/>
+                    <HomeTransactions balance={11.19} type={'negative'} acquired={'Restauracja Wartka'} place={'Tarnów 33-100, ul. Rynek 2'}/>
+                    <HomeTransactions balance={23.18} type={'negative'} acquired={'Łukasz Babicz. Usługi ogrodnicze'} place={'Tarnów 33-101, ul. Królowej Jadwigi 32'}/>
+                    <HomeTransactions balance={950.00} type={'positive'} acquired={'Stypendium naukowe'} place={'Tarnów 33-100, ul. Mickiewicza 8'}/>
+                    <HomeTransactions balance={21.37} type={'negative'} acquired={'PSB MARKA'} place={'Kraków 30-394, ul. Skotnicka 211'}/>
                 </div>
             </div>
         </div>
