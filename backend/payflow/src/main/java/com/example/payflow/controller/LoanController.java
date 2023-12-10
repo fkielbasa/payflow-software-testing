8b7998859b5239c8ba444ceda21df840ccb5dc79
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2023-12-10 14:22:25 +0100

    add getAll method

--- backend/payflow/src/main/java/com/example/payflow/controller/LoanController.java
+++ backend/payflow/src/main/java/com/example/payflow/controller/LoanController.java
@@ -1,6 +1,7 @@
 package com.example.payflow.controller;
 
 import com.example.payflow.loan.Loan;
+import com.example.payflow.loan.LoanRepository;
 import com.example.payflow.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 public class LoanController {
 
     private final LoanService loanService;
+    private final LoanRepository loanRepository;
 
     @Autowired
-    public LoanController(LoanService loanService) {
+    public LoanController(LoanService loanService, LoanRepository loanRepository) {
         this.loanService = loanService;
+        this.loanRepository = loanRepository;
+    }
+    @GetMapping("/loans")
+    public List<Loan> getAllLoans() {
+        return loanRepository.findAll();
     }
 
-    @GetMapping("/loans/{id}")
+    @GetMapping("/loan/{id}")
     public ResponseEntity<Loan> getLoanById(@PathVariable Long id) {
         Loan loan = loanService.getLoanById(id);
         return ResponseEntity.ok(loan);
