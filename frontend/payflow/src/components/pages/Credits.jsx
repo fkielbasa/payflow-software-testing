b140238782ada138d64f7303d892c910ee956eee
Narothe <kuba.dojka@gmail.com>
2023-12-17 23:35:06 +0100

    expression of fatigue and frustration

--- frontend/payflow/src/components/pages/Credits.jsx
+++ frontend/payflow/src/components/pages/Credits.jsx
@@ -4,7 +4,11 @@ import "../styles/credits/CreditsStyles.css";
 function Credits() {
     return (
         <div className="credits-container">
-            <h2>Strona z kredytami</h2>
+            <h2>Weź se kurwa kredyt</h2>
+            <h4>spierdalaj</h4>
+            <h2>kredyt przyznano na 30tyś złotych, z poważaniem IPKO</h2>
+            <h1>CHUJ WAM W DUPE</h1>
+            <h4>a tak serio, nie mam pmysłu co tu może być, obgadajmy to, idę spać</h4>
         </div>
     );
 }
