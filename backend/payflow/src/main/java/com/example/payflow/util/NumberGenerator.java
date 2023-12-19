4cb4442cf40007c8480aeb5e401eb4ec775055ce
enqer <pawel.kam4@gmail.com>
2023-12-19 22:47:08 +0100

    fix: name tables and validation

--- backend/payflow/src/main/java/com/example/payflow/util/NumberGenerator.java
+++ backend/payflow/src/main/java/com/example/payflow/util/NumberGenerator.java
@@ -1,15 +1,18 @@
 package com.example.payflow.util;
 
+
 import java.util.Random;
+
 
 public class NumberGenerator {
 
     public static final int BLIK_LENGTH = 6;
     public static final int LOGIN_LENGTH = 8;
     public static final int ACCOUNT_NUMBER_LENGTH = 26;
-    private static Validation validation;
     private static Random random;
     private static StringBuilder stringBuilder;
+
+
 
     public static String generateBlikCode(){
         random = new Random();
@@ -28,26 +31,21 @@ public class NumberGenerator {
             int randomNumber = random.nextInt(10); // 0-9
             stringBuilder.append(randomNumber);
         }
-        if (!validation.isLoginValid(stringBuilder.toString())){
-            return generateLogin();
-        }
         return stringBuilder.toString();
     }
 
     public static String generateAccountNumber(){
         random = new Random();
         stringBuilder = new StringBuilder();
-
         int firstDigit = random.nextInt(9) + 1;
         stringBuilder.append(firstDigit);
-
         for (int i = 0; i < ACCOUNT_NUMBER_LENGTH-1; i++) {
             int randomNumber = random.nextInt(10);
             stringBuilder.append(randomNumber);
         }
-        if (!validation.isAccountNumberValid(stringBuilder.toString())){
-            return generateAccountNumber();
-        }
         return stringBuilder.toString();
     }
+
+
+
 }
