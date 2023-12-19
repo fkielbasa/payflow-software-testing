3a196da6947635549734f231dbb5b7a5cefc430f
Narothe <kuba.dojka@gmail.com>
2023-12-19 18:01:17 +0100

    Repair Kuba elements + change Kuba Home

--- frontend/payflow/src/components/pages/Home.jsx
+++ frontend/payflow/src/components/pages/Home.jsx
@@ -12,24 +12,24 @@ function Home() {
             {/*<TopNavbar/>*/}
             <div className="contentWrapper">
                 <div className="leftSidePosition">
-                    <div className="balancePosition">
-                        <CreditCard currency={'USD'} type={'Karta bankomatowa'} balance={21.37}
-                                    accountNumber={'3423870000441246075444'} Owner={'Johnny Johinnson'}
-                                    expirationMonth={4} expirationYear={24} cardStyle={1} details={true} graph={false}/>
-                        <div className="balancePosition properMargin">
-                            <CreditCard currency={'EUR'} type={'Karta bankomatowa'} balance={69.42}
-                                        accountNumber={'3124150000843646841238'} Owner={'Johnny Johinnson'}
-                                        expirationMonth={7} expirationYear={25} cardStyle={2} details={true}
-                                        graph={false}/>
-                            <div className="balancePosition properMargin">
-                                <CreditCard currency={'PLN'} type={'Karta kredytowa'} balance={1069.42}
-                                            accountNumber={'4287931654718293458712'} Owner={'Johnny Johinnson'}
-                                            expirationMonth={12} expirationYear={27} cardStyle={4} details={true}
-                                            graph={false}/>
+                    {/*<div className="balancePosition">*/}
+                    {/*    <CreditCard currency={'USD'} type={'Karta bankomatowa'} balance={21.37}*/}
+                    {/*                accountNumber={'3423870000441246075444'} Owner={'Johnny Johinnson'}*/}
+                    {/*                expirationMonth={4} expirationYear={24} cardStyle={1} details={true} graph={false}/>*/}
+                    {/*    <div className="balancePosition properMargin">*/}
+                    {/*        <CreditCard currency={'EUR'} type={'Karta bankomatowa'} balance={69.42}*/}
+                    {/*                    accountNumber={'3124150000843646841238'} Owner={'Johnny Johinnson'}*/}
+                    {/*                    expirationMonth={7} expirationYear={25} cardStyle={2} details={true}*/}
+                    {/*                    graph={false}/>*/}
+                    {/*        <div className="balancePosition properMargin">*/}
+                    {/*            <CreditCard currency={'PLN'} type={'Karta kredytowa'} balance={1069.42}*/}
+                    {/*                        accountNumber={'4287931654718293458712'} Owner={'Johnny Johinnson'}*/}
+                    {/*                        expirationMonth={12} expirationYear={27} cardStyle={4} details={true}*/}
+                    {/*                        graph={false}/>*/}
 
-                            </div>
-                        </div>
-                    </div>
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
                 <div className="transactionPosition">
                     <div className="shortPayment">
--- frontend/payflow/src/index.js
+++ frontend/payflow/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import StartPage from "./components/startFrontPages/startPage";
-import DefaultPage from "./components/startFrontPages/defaultPage";
-import StartAccounts from "./components/startFrontPages/startAccounts";
-import StartCredits from "./components/startFrontPages/startCredits";
-import StartCards from "./components/startFrontPages/startCards";
-import StartSecure from "./components/startFrontPages/startSecure";
-import StartServices from "./components/startFrontPages/startServices";
-import Login from "./components/login/login";
-import Register from "./components/login/register";
+
+// import StartPage from "./components/startFrontPages/startPage";
+// import DefaultPage from "./components/startFrontPages/defaultPage";
+// import StartAccounts from "./components/startFrontPages/startAccounts";
+// import StartCredits from "./components/startFrontPages/startCredits";
+// import StartCards from "./components/startFrontPages/startCards";
+// import StartSecure from "./components/startFrontPages/startSecure";
+// import StartServices from "./components/startFrontPages/startServices";
+// import Login from "./components/login/login";
+// import Register from "./components/login/register";
 import Home from "./components/pages/Home";
 import Transactions from "./components/pages/Transactions";
 import Transfers from "./components/pages/Transfers";
@@ -40,21 +41,22 @@ ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                {/*<Route path="/" element={<App />} />*/}
-                <Route path="/" element={<StartPage />}>
-                    <Route path="/" element={<DefaultPage />} />
-                    <Route path="/accounts" element={<StartAccounts />} />
-                    <Route path="/credits" element={<StartCredits />} />
-                    <Route path="/cards" element={<StartCards />} />
-                    <Route path="/services" element={<StartServices />} />
-                    <Route path="/secure" element={<StartSecure />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Route>
+                <Route path="/" element={<App/>}>
+
+                {/*<Route path="/" element={<StartPage />}>*/}
+                {/*    <Route path="/" element={<DefaultPage />} />*/}
+                {/*    <Route path="/accounts" element={<StartAccounts />} />*/}
+                {/*    /!*<Route path="/credits" element={<StartCredits />} />*!/*/}
+                {/*    /!*<Route path="/cards" element={<StartCards />} />*!/*/}
+                {/*    <Route path="/services" element={<StartServices />} />*/}
+                {/*    <Route path="/secure" element={<StartSecure />} />*/}
+                {/*    <Route path="/login" element={<Login />} />*/}
+                {/*    <Route path="/register" element={<Register />} />*/}
+                {/*</Route>*/}
 
                     {/*<Route path="*" element={<NotFound/>}/>*/}
 
-                <Route path="/home" element={<App/>}>
+                {/*<Route path="/home" element={<App/>}>*/}
                     <Route path="/home" element={<Home/>} />
                     <Route path="transactions" element={<Transactions/>} />
                     <Route path="transfers" element={<Transfers/>} />
