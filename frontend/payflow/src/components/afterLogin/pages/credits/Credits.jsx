327a00861c92c4056f1e4035748f71bf2926f8a3
Narothe <kuba.dojka@gmail.com>
2024-01-03 00:34:17 +0100

    feat: add new forms
    style: change some styles

--- frontend/payflow/src/components/afterLogin/pages/credits/Credits.jsx
+++ frontend/payflow/src/components/afterLogin/pages/credits/Credits.jsx
@@ -14,20 +14,48 @@ function Credits() {
                 <h3>Formularz przyznania kredytu</h3>
                 <form className={styles.formStyle} onSubmit={handleSubmit}>
                     <div className={styles.textJustify}>
-                        <p className={styles.textMargin}>Dane identyfikacyjne:</p>
+                        <p className={styles.hardText}>Dane identyfikacyjne:</p>
                     </div>
-                    <GeneralForm showText={"Numer wniosku"} name={"applicationNumber"} type={"number"} maxLength={26} minLength={0} required={true}/>
-                    <GeneralForm showText={"Miejscowość"} name={"day"} type={"text"} maxLength={40} minLength={0} required={true}/>
-                    <GeneralForm showText={"Dzień"} name={"day"} type={"date"} maxLength={40} minLength={0} required={true}/>
-                    <hr className={styles.hrLine}/>
+                    <GeneralForm showText={"Numer wniosku"} name={"applicationNumber"} type={"number"} maxLength={26}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Miejscowość"} name={"place"} type={"text"} maxLength={40} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Dzień"} name={"day"} type={"date"} maxLength={40} minLength={0}
+                                 required={true}/>
+                    <hr className={styles.hardHrLine}/>
                     <div className={styles.textJustify}>
-                        <p className={styles.textMargin}>Dane kredytobiorcy:</p>
+                        <p className={styles.hardText}>Dane kredytobiorcy:</p>
                     </div>
-                    <GeneralForm showText={"Nazwisko"} name={"surname"} type={"text"} maxLength={40} minLength={0} required={true}/>
-                    <GeneralForm showText={"Imie"} name={"name"} type={"text"} maxLength={40} minLength={0} required={true}/>
-                    <GeneralForm showText={"Drugie imie"} name={"secondName"} type={"text"} maxLength={40} minLength={0} required={false}/>
-                    <GeneralForm showText={"PESEL"} name={"pesel"} type={"number"} maxLength={11} minLength={0} required={true}/>
-
+                    <GeneralForm showText={"Nazwisko"} name={"surname"} type={"text"} maxLength={40} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Imie"} name={"name"} type={"text"} maxLength={40} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Drugie imie"} name={"secondName"} type={"text"} maxLength={40} minLength={0}
+                                 required={false}/>
+                    <GeneralForm showText={"PESEL"} name={"pesel"} type={"number"} maxLength={11} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Nr. telefonu"} name={"phoneNumber"} type={"number"} maxLength={9} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Adres email"} name={"email"} type={"text"} maxLength={40} minLength={0}
+                                 required={true}/>
+                    <GeneralForm showText={"Dokument tożsamości"} name={"identityCard"} type={"text"} maxLength={9}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Ważny do"} name={"identityCardExpired"} type={"date"} maxLength={40}
+                                 minLength={0} required={true}/>
+                    <hr className={styles.lightHrLine}/>
+                    <div className={styles.textJustify}>
+                        <p className={styles.lightText}>Dane zamieszkania:</p>
+                    </div>
+                    <GeneralForm showText={"Kod pocztowy"} name={"zipCode"} type={"text"} maxLength={10}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Miejscowość"} name={"residence"} type={"text"} maxLength={40}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Ulica"} name={"street"} type={"text"} maxLength={40}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Nr. domu/mieszkania"} name={"houseApartmentNumber"} type={"number"} maxLength={40}
+                                 minLength={0} required={true}/>
+                    <GeneralForm showText={"Okres zamieszkania od"} name={"residenceFrom"} type={"date"} maxLength={40}
+                                 minLength={0} required={true}/>
                     <div className={styles.buttonJustify}>
                         <input className={styles.submit} type="submit" value="Prześlij"/>
                     </div>
