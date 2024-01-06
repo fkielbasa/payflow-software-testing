9b885ab487eabb4849cc486274f349c6af025a80
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-01-06 22:18:58 +0100

    fix: resolve conflicts

--- backend/payflow/src/main/java/com/example/payflow/dto/CardDTO.java
+++ backend/payflow/src/main/java/com/example/payflow/dto/CardDTO.java
@@ -1,19 +1,29 @@
 package com.example.payflow.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
 
 import java.time.LocalDate;
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class CardDTO {
-    Long id;
-    String cardNumber;
-    LocalDate validDate;
-    String cvv;
+    @NonNull
+    private Long id;
+    @NonNull
+    private String cardNumber;
+    @NonNull
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate validDate;
+    @NonNull
+    private String cvv;
+    private boolean isActive;
+    private boolean isBlocked;
 }
-
