ca0e258ccbc9cfc747d9f299a2fcf4c00abfdf3a
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-01-13 20:48:27 +0100

    feat: change user's address, enum for address type

--- backend/payflow/src/main/java/com/example/payflow/controller/UserDetailsController.java
+++ backend/payflow/src/main/java/com/example/payflow/controller/UserDetailsController.java
@@ -4,7 +4,7 @@ import com.example.payflow.dto.EmailDTO;
 import com.example.payflow.dto.PhoneNumberDTO;
 import com.example.payflow.service.UserDetailsServices;
 import com.example.payflow.model.UserDetails;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class UserDetailsController {
 
-    private final UserDetailsServices userDetailsService;
+    private final UserDetailsServices userDetailsServices;
 
 //    @GetMapping("/users/{id}/details")
 //    public ResponseEntity<UserDetails> getUserDetailsById(@PathVariable Long id) {
@@ -23,14 +23,14 @@ public class UserDetailsController {
 //    }
     @PatchMapping("/user/{id}/email")
     public ResponseEntity<?> changeUserEmail(@PathVariable Long id, @RequestBody EmailDTO email){
-        UserDetails userDetails = userDetailsService.changeUserEmail(id,email);
+        UserDetails userDetails = userDetailsServices.changeUserEmail(id,email);
         if(userDetails != null)
             return new ResponseEntity("Email successfully changed.", HttpStatus.OK);
         return new ResponseEntity("Invalid body or email already exists.",HttpStatus.BAD_REQUEST);
     }
     @PatchMapping("/user/{id}/phone-number")
     public ResponseEntity<?> changeUserPhoneNumber(@PathVariable Long id, @RequestBody PhoneNumberDTO number){
-        UserDetails userDetails = userDetailsService.changeUserPhoneNumber(id,number);
+        UserDetails userDetails = userDetailsServices.changeUserPhoneNumber(id,number);
         if(userDetails != null)
             return new ResponseEntity("Phone number successfully changed.", HttpStatus.OK);
         return new ResponseEntity("Invalid body or phone number already exists.",HttpStatus.BAD_REQUEST);
