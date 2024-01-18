01cc75c43d69bcdc299a978a07a8463ed50e9fec
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-01-18 11:19:09 +0100

    refactor: replace request body with path variable id in create card

--- backend/payflow/src/main/java/com/example/payflow/controller/CardController.java
+++ backend/payflow/src/main/java/com/example/payflow/controller/CardController.java
@@ -1,14 +1,12 @@
 package com.example.payflow.controller;
 
 import com.example.payflow.dto.CardDTO;
-import com.example.payflow.dto.CardDTOPost;
 import com.example.payflow.model.Card;
 import com.example.payflow.service.CardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,15 +23,15 @@ public class CardController {
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-    @PostMapping("/card")
-    public ResponseEntity<CardDTO> createCard(@RequestBody CardDTOPost card){
-        CardDTO c = cardService.createCard(card);
+    @PostMapping("/numbers/{id}/card")
+    public ResponseEntity<CardDTO> createCard(@PathVariable Long id){
+        CardDTO c = cardService.createCard(id);
         if(c != null){
             return ResponseEntity.status(HttpStatus.CREATED).body(c);
         }
         return new ResponseEntity("Invalid data or account doesn't exist",HttpStatus.BAD_REQUEST);
     }
-    @DeleteMapping("/card/{id}")
+    @DeleteMapping("/cards/{id}")
     public ResponseEntity<String> removeCardById(@PathVariable Long id){
         Card c = cardService.removeCardById(id);
         if (c != null){
