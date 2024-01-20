f7b8d8f5413daa9fbe3e4db0459b495931ab5aa1
enqer <pawel.kam4@gmail.com>
2024-01-20 23:42:15 +0100

    feat: cantor exchange

--- frontend/src/components/afterLogin/pages/cantor/cantor.jsx
+++ frontend/src/components/afterLogin/pages/cantor/cantor.jsx
@@ -1,9 +1,38 @@
+import CantorExchange from "./cantorExchange";
+import {useEffect, useMemo, useState} from "react";
+import axios from "axios";
+import {config} from "../../../../config/authConfig";
+import {BASE_URL} from "../../../../config/shared";
+import ExchangeChart from "./exchangeChart";
+import styles from './cantor.module.css'
+
 
 const Cantor = () => {
+    const [dataRate, setDataRate] = useState([])
+
+    useEffect(() => {
+        const getExchangeRates = () => {
+            axios
+                .get(
+                    `${BASE_URL}/api/v1/exchange-rates?last=${7}`,
+                    config
+                )
+                .then(r => {
+                    setDataRate(r.data)
+                    console.log(r.data)
+                })
+                .catch(er => console.log("Couldn't get rates"))
+        }
+        getExchangeRates()
+    }, []);
+
 
     return(
-        <div>
-
+        <div className={styles.containerFluid}>
+            <div className={styles.exchangeBar}>
+                <CantorExchange />
+            </div>
+            
         </div>
     )
 }
